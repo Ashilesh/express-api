385f3a44be610036d2a618cbc079679225dd66da
Ashilesh Sonkusle <ashileshsonkusle1@gmail.com>
2022-11-03 01:36:36 +0530

    fix test case for invalid route

--- src/api-routes/invalid-route.js
+++ src/api-routes/invalid-route.js
@@ -4,7 +4,11 @@ import { ErrorResponse } from "../model/response/error-response.js";
 export const invalidRouter = Router()
 
 invalidRouter.use((req, res) => {
-    const msg = new ErrorResponse(["Invalid route"])
+    const msg = new ErrorResponse([{
+        "msg": "Invalid URL",
+        "param": req.url,
+        "location": "URL",
+    }])
     res.status(404).send(msg)
 })
 
